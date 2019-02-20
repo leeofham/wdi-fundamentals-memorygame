c317c62e0b752705cae8474e116cdf34d38f919f
Liam Roberts <liamroberts300@gmail.com>
2019-02-20 14:23:21 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,19 @@
 
 var cards = ["queen", "queen", "king", "king"];
-
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[3];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2){
-	cardCheck = cardsInPlay[0] === cardsInPlay[1] ? alert("You have found a match") : alert("Sorry, try again");
+var checkForMatch = function(){
+	if (cardsInPlay.length === 2){
+		cardCheck = cardsInPlay[0] === cardsInPlay[1] ? console.log("You have found a match") : console.log("Sorry, try again");
+	}
 }
+
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch()
+}
+
+flipCard(0)
+flipCard(2)
