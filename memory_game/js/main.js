f0cdbb01b1775c7f5738bda38dd61e22894bf147
Liam Roberts <liamroberts300@gmail.com>
2019-02-21 12:59:39 +0000

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,48 @@
+var cards = [
+{
+	rank: "Queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "Queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
-var cards = ["queen", "queen", "king", "king"];
+
+
 var cardsInPlay = [];
 
 var checkForMatch = function(){
 	if (cardsInPlay.length === 2){
-		cardCheck = cardsInPlay[0] === cardsInPlay[1] ? console.log("You have found a match") : console.log("Sorry, try again");
+		cardCheck = cardsInPlay[0] === cardsInPlay[1] ? alert("You have found a match") : alert("Sorry, try again");
 	}
 }
 
 var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 	checkForMatch()
+	
 }
 
 flipCard(0)
 flipCard(2)
+
+
